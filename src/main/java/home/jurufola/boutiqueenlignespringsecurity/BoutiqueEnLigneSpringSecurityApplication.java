a4bbcbfba2905a0c335cfb030dbd831010822d56
jurufola <moulaye.haidara@laposte.net>
2021-07-15 10:22:11 +0200

    Initial commit

---
+++ src/main/java/home/jurufola/boutiqueenlignespringsecurity/BoutiqueEnLigneSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package home.jurufola.boutiqueenlignespringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoutiqueEnLigneSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoutiqueEnLigneSpringSecurityApplication.class, args);
+	}
+
+}
